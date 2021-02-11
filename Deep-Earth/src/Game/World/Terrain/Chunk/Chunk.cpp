d078c7152e26ada46decf3b3a15de94d4a6f1e4e
AJP-9 <albertpjzzo@gmail.com>
2021-02-11 16:29:26 -0500

    set/get in chunk, & buildmesh update
    Added set/get for blocks in Chunk. And updated buildMesh in Chunk.

--- Deep-Earth/src/Game/World/Terrain/Chunk/Chunk.cpp
+++ Deep-Earth/src/Game/World/Terrain/Chunk/Chunk.cpp
@@ -2,13 +2,13 @@
 
 #include <algorithm>
 #include "../Block/Type/BaseBlockType.hpp"
-#include "../../../../Engine/Util/Math.hpp"
 #include <iostream>
 Chunk::Chunk(BlockManager& blockManager, glm::ivec3 position)
 	: m_TransformationMatrix(engine::math::TransMatrix::createTransformationMatrix(position * CHUNK_SIZE))
 {
-	for (int x = 0; x < CHUNK_VOLUME; x++)
-		m_Blocks.at(x) = blockManager.getRawBlock(block::dirt);
+	setBlock(glm::ivec3(0), block::sand);
+	//for (int x = 0; x < CHUNK_VOLUME; x++)
+	//	m_Blocks.at(x) = blockManager.getRawBlock(block::dirt);
 
 	buildMesh(blockManager);
 }
@@ -25,14 +25,41 @@ void Chunk::render(engine::Shader3D& shader)
 	m_VertexArray->render();
 }
 
+inline RawBlock& Chunk::getBlock(glm::ivec3 position)
+{
+	uint flat = engine::math::DimensionalAndFlat::getFlatFrom3D(position, CHUNK_SIZE);
+
+	if (flat >= CHUNK_VOLUME || flat < 0)
+	{
+		std::cout << "ERROR::<Chunk::getBlock> Out of bounds exception!\n";
+	}
+	else
+	{
+		return m_Blocks.at(flat);
+	}
+}
+
+inline void Chunk::setBlock(glm::ivec3 position, uint id)
+{
+	uint flat = engine::math::DimensionalAndFlat::getFlatFrom3D(position, CHUNK_SIZE);
+
+	if (flat >= CHUNK_VOLUME || flat < 0)
+	{
+		std::cout << "ERROR::<Chunk::getBlock> Out of bounds exception!\n";
+	}
+	else
+	{
+		m_Blocks.at(flat) = RawBlock(id);
+	}
+}
+
 void Chunk::buildMesh(BlockManager& blockManager)
 {
 	std::vector<engine::Vertex> vertices;
 	std::vector<uint> indices;
 
-	uint greatestIndex = -1;
-
 	uint indx = -1;
+	uint greatestIndex = -1;
 	for (auto& b : m_Blocks)
 	{
 		++indx;
@@ -41,6 +68,16 @@ void Chunk::buildMesh(BlockManager& blockManager)
 		
 		glm::ivec3 bPosition = engine::math::DimensionalAndFlat::get3DFromFlat(indx, CHUNK_SIZE);
 
+		// front
+		//if()
+		// front-right
+		// back
+		// front-left
+		// top
+		// bottom
+
+		//std::cout << engine::math::DimensionalAndFlat::getFlatFrom3D(glm::ivec3(31, 31, 31), 32) << std::endl;
+		//std::cout << engine::math::DimensionalAndFlat::get3DFromFlat(32767, 32).x << ", " << engine::math::DimensionalAndFlat::get3DFromFlat(32767, 32).y << ", " << engine::math::DimensionalAndFlat::get3DFromFlat(32767, 32).z << std::endl;
 
 		std::vector<engine::Vertex> bVertices = blockManager.getBlockType(b.m_ID)->getVertices(bPosition); // you have to transform by sending the coords thru getVtx
 		std::vector<uint> bIndices = blockManager.getBlockType(b.m_ID)->getIndices(greatestIndex);
@@ -48,7 +85,7 @@ void Chunk::buildMesh(BlockManager& blockManager)
 		vertices.insert(vertices.end(), bVertices.begin(), bVertices.end());
 		indices.insert(indices.end(), bIndices.begin(), bIndices.end());
 
-		greatestIndex = *std::max_element(bIndices.begin(), bIndices.end());
+		greatestIndex = *std::max_element(bIndices.begin(), bIndices.end()); // optimize this
 	}
 
 	m_VertexArray = std::make_unique<engine::gl::VertexArray>(vertices, indices);
--- Deep-Earth/src/Game/World/Terrain/Chunk/Chunk.hpp
+++ Deep-Earth/src/Game/World/Terrain/Chunk/Chunk.hpp
@@ -5,9 +5,11 @@
 #include "../../../../Engine/OpenGL/VertexArray.hpp"
 #include "../../../../Engine/Renderer/Shader/Shader3D.hpp"
 #include "../Block/BlockManager.hpp"
+#include "../../../../Engine/Util/Math.hpp"
+#include <iostream>
 
 #define CHUNK_SIZE 32
-#define CHUNK_VOLUME 32*32*32
+#define CHUNK_VOLUME 32768 // 32*32*32
 
 class Chunk
 {
@@ -18,9 +20,12 @@ public:
 
 	void render(engine::Shader3D& shader);
 
-	void buildMesh(BlockManager& blockManager);
+	RawBlock& getBlock(glm::ivec3 position);
+	void setBlock(glm::ivec3 position, uint id);
 
 private:
+
+	void buildMesh(BlockManager& blockManager);
 
 	std::unique_ptr<engine::gl::VertexArray> m_VertexArray;
 
